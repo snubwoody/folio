faa8aa16e4a995bce11a6dcf717d2b3ec1271e57
waku <wakunguma13@gmail.com>
2025-10-13 23:35:14 -0600

    fix: Add tailwind to vite

--- src/components/TransactionTable.svelte
+++ src/components/TransactionTable.svelte
@@ -11,25 +11,30 @@
 	});
 </script>
 
-<section>
+<ul class="expense-table">
 	<p class="table-heading">Category</p>
 	<p class="table-heading">Account</p>
 	<p class="table-heading">Date</p>
 	<p class="table-heading">Amount</p>
 
-	<p>Category</p>
-	<p>Account</p>
-	<p>Date</p>
-	<p>Amount</p>
-</section>
+	<p class="table-cell">Category</p>
+	<p class="table-cell">Account</p>
+	<p class="table-cell">Date</p>
+	<p class="table-cell">Amount</p>
+</ul>
 
 <style>
-	section{
+	.expense-table{
 		display: grid;
 		grid-template-columns: repeat(4,1fr);
 	}	
 
 	.table-heading{
 		color: var(--color-text-muted);
+	}
+
+	.table-cell{
+		border-left: 1px solid var(--color-neutral-50);
+		border-bottom: 1px solid var(--color-neutral-50);
 	}
 </style>
--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,5 +1,21 @@
-type Account = {
+export type Account = {
 	id: string,
 	name: string,
+	currencyCode: string,
 	startingBalance: number
+}
+
+export type Category = {
+	id: string,
+	title: string
+}
+
+export type Expense = {
+	id: string,
+	amount: number,
+	description: string,
+	category?: Category,
+	account?: Account,
+	date: string,
+	currencyCode: string
 }
