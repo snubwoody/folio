0175795b185f9b641869b3a13a04ae7031443d6c
waku <wakunguma13@gmail.com>
2025-10-15 11:54:04 -0600

    chore: Format and lint

--- src/components/DateField.svelte
+++ src/components/DateField.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { createDateField } from "@melt-ui/svelte";
-
 	type Props = {
 		onChange?: (year: number,month: number,day: number) => void
 	}
@@ -9,12 +7,11 @@
     // The date is always parsed as YYYY-MM-DD regardless of the locale.
     let selectedDate: string | undefined= $state(undefined);
 
-
     $effect(()=>{
         if (!selectedDate) return;
         let [year,month,day] = selectedDate.split("-").map(d => parseInt(d));
         onChange?.(year,month,day);
-    })
+    });
 </script>
 
 <label class="flex h-full w-full flex-col gap-1">
@@ -29,12 +26,18 @@
 		border: 1px solid var(--color-neutral-50);
 		padding: 8px 12px;
 		border-radius: var(--radius-sm);
+        outline: none;
+        transition: all 150ms;
+
+        &:focus{
+            border-color: var(--color-purple-500);
+        }
 	}
 
     /* Hide default calendar button */
     input[type="date"]::-webkit-inner-spin-button,
     input[type="date"]::-webkit-calendar-picker-indicator {
         display: none;
-        -webkit-appearance: none;
+        appearance: none;
     }
 </style>
