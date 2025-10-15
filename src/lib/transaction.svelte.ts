f310ec0149f76971f754bfdbaf3c3f34261f2fb0
waku <wakunguma13@gmail.com>
2025-10-15 10:58:00 -0600

    Switch transaction store to class

--- src/lib/transaction.svelte.ts
+++ src/lib/transaction.svelte.ts
@@ -37,61 +37,64 @@ export type Income = {
 	currencyCode: string
 }
 
-export const transactionStore = createTransactionStore();
 
-export function createTransactionStore(){
-    let expenses: Expense[] = $state([]);
-    let incomes: Income[] = $state([]);
-    let categories: Category[] = $state([]);
-    let incomeStreams: IncomeStream[] = $state([]);
-    return {
-        get expenses(){
-            return expenses;
-        },
-        get incomes(){
-            return incomes;
-        },
-        get categories(){
-            return categories;
-        },
-        get incomeStreams(){
-            return incomeStreams;
-        },
-        async editExpense(opts: EditExpense){
-            const { id,...data } = opts;
-            await invoke("edit_expense",{ id,data });
-            await this.load();
-        },
-        async addExpense({
+export class TransactionStore{
+    #expenses: Expense[] = $state([]);
+    #incomes: Income[] = $state([]);
+    #categories: Category[] = $state([]);
+    #incomeStreams: IncomeStream[] = $state([]);
+
+    get expenses(): Expense[]{
+        return this.#expenses;
+    }
+    
+    get incomes(): Income[]{
+        return this.#incomes;
+    }
+    
+    get incomeStreams(): IncomeStream[]{
+        return this.#incomeStreams;
+    }
+    get categories(): IncomeStream[]{
+        return this.#categories;
+    }
+    async editExpense(opts: EditExpense){
+        const { id,...data } = opts;
+        await invoke("edit_expense",{ id,data });
+        await this.load();
+    }
+    async addExpense(opts:CreateExpense){
+        const {
             amount = "0",
             accountId,
             categoryId,
             currencyCode = "USD",
             date,
-        }:CreateExpense,
-        ){
-            // TODO: use default currency code
-            const data: CreateExpense = {
-                amount,
-                accountId,
-                currencyCode,
-                categoryId,
-                date,
-            };
+        } = opts;
+        // TODO: use default currency code
+        const data: CreateExpense = {
+            amount,
+            accountId,
+            currencyCode,
+            categoryId,
+            date,
+        };
 
-            try{
-                await invoke("create_expense",{ data });
-            }
-            catch(e){
-                console.error(e);
-            }
-            await this.load();
-        },
-        async load(){
-            expenses = await invoke("fetch_expenses") as Expense[];
-            categories = await invoke("fetch_categories") as Category[];
-            incomes = await invoke("fetch_incomes") as Income[];
-            incomeStreams = await invoke("fetch_income_streams") as IncomeStream[];
-        },
-    };
+        try{
+            await invoke("create_expense",{ data });
+        }
+        catch(e){
+            console.error(e);
+        }
+        await this.load();
+    }
+        
+    async load(){
+        this.#expenses = await invoke("fetch_expenses") as Expense[];
+        this.#categories = await invoke("fetch_categories") as Category[];
+        this.#incomes = await invoke("fetch_incomes") as Income[];
+        this.#incomeStreams = await invoke("fetch_income_streams") as IncomeStream[];
+    }
 }
+
+export const transactionStore = new TransactionStore();
