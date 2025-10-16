ff7ad0216572f6876949cba1d1cd815293c1a232
waku <wakunguma13@gmail.com>
2025-10-16 09:08:45 -0600

    feat: Add income streams

--- src/lib/analytics.svelte.ts
+++ src/lib/analytics.svelte.ts
@@ -1,20 +1,30 @@
 import { invoke } from "@tauri-apps/api/core";
-import type { Category } from "./transaction.svelte";
+import type { Category, IncomeStream } from "./transaction.svelte";
 
 export type SpendingAnalytic = {
     category: Category,
     total: string
 }
 
+export type IncomeAnalytic = {
+    stream: IncomeStream,
+    total: string
+}
+
 export class AnalyticsStore{
     #spendingAnaltics: SpendingAnalytic[] = $state([]);
+    #incomeAnalytics: IncomeAnalytic[] = $state([]);
 
     get spendingAnalytics(): SpendingAnalytic[]{
         return this.#spendingAnaltics;
     }
+    
+    get incomeAnalytics(): IncomeAnalytic[]{
+        return this.#incomeAnalytics;
+    }
 
     async load(){
-        this.#spendingAnaltics = await invoke("spending_analytics") as SpendingAnalytic[];
+        this.#incomeAnalytics = await invoke("income_analytics") as IncomeAnalytic[];
     }
 }
 
--- src/routes/analytics/+page.svelte
+++ src/routes/analytics/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    import IncomeAnalytics from "$components/analytics/IncomeAnalytics.svelte";
     import SpendingAnalytics from "$components/analytics/SpendingAnalytics.svelte";
 </script>
 
 <main class="space-y-5 px-4 py-7">
+    <IncomeAnalytics/>
     <SpendingAnalytics/>
 </main>
