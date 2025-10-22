adbdb051afa5cd0f24535dd52fe0d1407bc5c229
Wakunguma K. <67170475+snubwoody@users.noreply.github.com>
2025-10-21 20:55:58 -0600

    Add GPL 3.0 license (#44)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,17 @@
+// Copyright (C) 2025 Wakunguma Kalimukwa
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we use adapter-static with a fallback to index.html to put the site in SPA mode
 // See: https://svelte.dev/docs/kit/single-page-apps
