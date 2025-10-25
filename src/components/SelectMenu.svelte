ad8402c75f7be7d99ca36b68359e2648fce3cfbe
waku <wakunguma13@gmail.com>
2025-10-24 18:29:15 -0600

    Start moving stuff to select component

--- src/components/SelectMenu.svelte
+++ src/components/SelectMenu.svelte
@@ -43,27 +43,35 @@ from.
 <script lang="ts" generics="T">
     import { useSelect, type SelectOption } from "$lib/select.svelte";
 
-	// TODO: add default value
-	type Props = {
-		label?: string,
-		items: T[],
-		toOption: (item: T) => SelectOption,
-		defaultValue?: T,
-		onChange?: (item: T) => void
-	}
+    // TODO: add default value
+    type Props = {
+        label?: string;
+        items: T[];
+        toOption: (item: T) => SelectOption;
+        defaultValue?: T;
+        onChange?: (item: T) => void;
+    };
 
-	const { label = "Label", items, toOption,onChange,defaultValue }: Props = $props();
+    const {
+        label = "Label",
+        items,
+        toOption,
+        onChange,
+        defaultValue,
+    }: Props = $props();
 
-	let selectedOption: SelectOption | undefined = $state(defaultValue ? toOption(defaultValue) : undefined);
+    let selectedOption: SelectOption | undefined = $state(
+        defaultValue ? toOption(defaultValue) : undefined,
+    );
 
-    function updateValue(item: T,option: SelectOption){
-	    selectedOption = option;
-	    if(item){
-	        onChange?.(item);
-	    }
+    function updateValue(item: T, option: SelectOption) {
+        selectedOption = option;
+        if (item) {
+            onChange?.(item);
+        }
     }
 
-    const { select,options } = useSelect({
+    const { select, options } = useSelect({
         onChange: ({ item, option }) => updateValue(item, option),
         items: items,
         toOption: toOption,
@@ -71,36 +79,36 @@ from.
 </script>
 
 <div class="space-y-1">
-	<p class="text-sm text-text-muted">{label}</p>
-	<button
-		{...select.trigger}
-		class="flex w-full items-center justify-between px-1.5 py-1 rounded-md border border-neutral-50"
-	>
-		<p>{selectedOption?.label ?? "Select an option"}</p>
-		<i class="ph ph-caret-down"></i>
-	</button>
+    <p class="text-sm text-text-muted">{label}</p>
+    <button
+        {...select.trigger}
+        class="flex w-full items-center justify-between px-1.5 py-1 rounded-md border border-neutral-50"
+    >
+        <p>{selectedOption?.label ?? "Select an option"}</p>
+        <i class="ph ph-caret-down"></i>
+    </button>
 </div>
 <ul {...select.content} class="popup-overlay space-y-1">
-	{#each options as  option (option.value)}
-		{@const selected = select.isSelected(option)}
-		<li {...select.getOption(option)} data-selected={selected}>
-			{option.label}
-		</li>
-	{/each}
+    {#each options as option (option.value)}
+        {@const selected = select.isSelected(option)}
+        <li {...select.getOption(option)} data-selected={selected}>
+            {option.label}
+        </li>
+    {/each}
 </ul>
 
 <style>
-	li{
-		border-radius: var(--radius-sm);
-		padding: 8px;
-		transition: all 250ms;
+    li {
+        border-radius: var(--radius-sm);
+        padding: 8px;
+        transition: all 250ms;
 
-		&:hover{
-			background-color: var(--color-neutral-50);
-		}
+        &:hover {
+            background-color: var(--color-neutral-50);
+        }
 
-		&[data-selected="true"]{
-			background-color: var(--color-neutral-50);
-		}
-	}
+        &[data-selected="true"] {
+            background-color: var(--color-neutral-50);
+        }
+    }
 </style>
