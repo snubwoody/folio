88774a2e8a31d55738b3d7a87f7b50c51d433d2d
waku <wakunguma13@gmail.com>
2025-10-31 12:40:42 -0600

    chore: fix lint warnings

--- packages/desktop/src/components/analytics/BudgetTable.svelte
+++ packages/desktop/src/components/analytics/BudgetTable.svelte
@@ -17,7 +17,6 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 <script lang="ts">
     import { appStore } from "$lib/state.svelte";
     import Budget from "./Budget.svelte";
-    $inspect(appStore.budgets);
 </script>
 
 <ul class="expense-table">
