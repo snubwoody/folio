9d4be1f2476ade0571aca795ea25a7f47df36cb9
Wakunguma K. <67170475+snubwoody@users.noreply.github.com>
2025-11-02 18:40:36 -0700

    feat(web): add footer and info section on home page (#78)

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 import node from "@astrojs/node";
-import process from "node:process";
 
 // https://astro.build/config
 export default defineConfig({
