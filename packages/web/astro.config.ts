bf16dc5cea4c05204df48a37668e722fddb0c694
Wakunguma K. <67170475+snubwoody@users.noreply.github.com>
2025-11-08 13:34:15 -0700

    (web): deploy to github pages (#93)

--- packages/web/astro.config.ts
+++ packages/web/astro.config.ts
@@ -2,17 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-import node from "@astrojs/node";
 
 export default defineConfig({
     prefetch: true,
+    output: "static",
     integrations: [sitemap()],
     vite: {
-        // what a horrible language
         // @ts-expect-error for some reason ts can't see this
         plugins: [tailwindcss()]
-    },
-    adapter: node({
-        mode: "standalone"
-    })
+    }
 });
