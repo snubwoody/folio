a8279727fe2acf6ae2afd50db44d2ab3e0608a57
waku <wakunguma13@gmail.com>
2025-11-12 16:58:51 -0700

    fix lint warnings

--- packages/feedback-worker/src/index.ts
+++ packages/feedback-worker/src/index.ts
@@ -1,4 +1,3 @@
-import { App } from "octokit";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 
@@ -6,13 +5,13 @@ dotenv.config();
 
 // TODO: add logging library
 
-const appId = "2280940";
-const clientId = "Iv23libGSoaUaiaWkC3D";
+// const appId = "2280940";
+// const clientId = "Iv23libGSoaUaiaWkC3D";
 const apiVersion = "2022-11-28";
-const installationId = 94393199;
-const privateKey = process.env.WORKER_PRIVATE_KEY ?? "";
+// const installationId = 94393199;
+// const privateKey = process.env.WORKER_PRIVATE_KEY ?? "";
 
-const createJwt = (clientId: string,privateKey: string): string => {
+export const createJwt = (clientId: string,privateKey: string): string => {
     const now = Math.floor(Date.now() / 1000);
     const payload = {
         iat: now,
@@ -20,20 +19,20 @@ const createJwt = (clientId: string,privateKey: string): string => {
         iss: clientId
     };
 
-    return jwt.sign(payload,privateKey,{algorithm: "RS256"});
+    return jwt.sign(payload,privateKey,{ algorithm: "RS256" });
 };
 
-const jwtToken = createJwt(clientId,privateKey);
+// const jwtToken = createJwt(clientId,privateKey);
 
 interface AccessTokenResponse{
     token: string,
     expires_at: string,
-} 
+}
 
 /**
  * Request an installation access token.
- * 
- * [Api docs](https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#create-an-installation-access-token-for-an-app) 
+ *
+ * [Api docs](https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#create-an-installation-access-token-for-an-app)
  */
 export const getAccessToken = async(installationId: number, jwtToken: string): Promise<string> => {
     const url = `https://api.github.com/app/installations/${installationId}/access_tokens`;
@@ -49,21 +48,19 @@ export const getAccessToken = async(installationId: number, jwtToken: string): P
     return json.token;
 };
 
-const accessToken = await getAccessToken(installationId,jwtToken);
+// const accessToken = await getAccessToken(installationId,jwtToken);
 
-
-
-interface CreateIssueBody{
-    title: string,
-    body?: string,
-    labels?: string[]
-}
+// interface CreateIssueBody{
+//     title: string,
+//     body?: string,
+//     labels?: string[]
+// }
 
 interface CreateIssueResponse{
     /** The url of the newly created issue. */
     url: string,
     repository_url: string,
-    id: number, 
+    id: number,
     title: string,
     body: string
 }
@@ -75,9 +72,9 @@ export const createIssue = async(accessToken: string) => {
             accept: "application/vnd.github.raw+json",
             "Authorization": `Token ${accessToken}`
         },
-        body:JSON.stringify({title: "[Bug report] App crashing"})
+        body:JSON.stringify({ title: "[Bug report] App crashing" })
     });
-    
+
     const response = await body.json() as CreateIssueResponse;
     console.log(response);
 };
