426caf9d008e41c4b8af1d7a89d17fccae3819ec
waku <wakunguma13@gmail.com>
2025-11-16 15:26:23 -0700

    add worker crate

--- migrations/20251014003657_init_categories.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-CREATE TABLE categories(
-	id TEXT PRIMARY KEY NOT NULL DEFAULT (hex(randomBlob(8))),
-	title TEXT NOT NULL
-);
--- migrations/20251024040938_add_created_at_to_income_streams.sql
+++
@@ -1,2 +0,0 @@
--- Add migration script here
-UPDATE income_streams SET created_at = unixepoch('now') WHERE created_at IS NULL;
