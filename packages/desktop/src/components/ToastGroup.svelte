412b9f921b885b9e264deaeb21a9dcd392f54e6c
waku <wakunguma13@gmail.com>
2025-11-25 15:32:28 -0700

    WIP

---
+++ packages/desktop/src/components/ToastGroup.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+    // TODO: close toast on esc
+</script>
+
+<div class="toast-group">
+    <div class="toast">
+        This is a toast
+    </div>
+</div>
+
+<style>
+    .toast-group{
+        position: fixed;
+    }
+
+    .toast{
+        position: fixed;
+    }
+</style>
---
+++ packages/desktop/src/lib/toast.svelte.ts
@@ -0,0 +1,5 @@
+
+export type ToastData = {
+    title: string,
+    body?: string,
+};
