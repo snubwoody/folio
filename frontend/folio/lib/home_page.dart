25edca2988b7756f8726bc521d96fd973cdf5f1b
waku <wakunguma13@gmail.com>
2026-02-06 23:06:46 -0700

    Add transaction table

--- frontend/folio/lib/home_page.dart
+++ frontend/folio/lib/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(24.0),
-      child: Column(spacing: 24,children: [AccountPanel(),TransactionPanel()]),
+      child: Column(spacing: 24,children: [AccountPanel(),Expanded(child: TransactionPanel())]),
     );
   }
 }
@@ -21,10 +21,83 @@ class TransactionPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: 24,
       children: [
         Row(children: [TextLabel("Transactions"), Spacer(),TextLabel("New")]),
+        TransactionTable(),
       ],
     );
+  }
+}
+
+class TransactionTableSource extends DataTableSource{
+  @override
+  int get rowCount => 2;
+
+  @override
+  DataRow? getRow(int index) {
+    switch (index){
+      case 0:
+        return DataRow(cells: [
+          DataCell(TextLabel("Name")),
+          DataCell(TextLabel("RBC")),
+          DataCell(TextLabel("2025-12-12")),
+          DataCell(TextLabel("500.00")),
+        ]);
+        case 1:
+        return DataRow(cells: [
+          DataCell(TextLabel("Name")),
+          DataCell(TextLabel("RBC")),
+          DataCell(TextLabel("2025-12-12")),
+          DataCell(TextLabel("500.00")),
+        ]);
+      default:
+        return null;
+    }
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => 0;
+}
+
+class TransactionTable extends StatelessWidget {
+  const TransactionTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [TableCell(),TableCell(),TableCell(),TableCell()],
+        ),
+        Row(
+          children: [TableCell(),TableCell(),TableCell(),TableCell()],
+        ),
+        Row(
+          children: [TableCell(),TableCell(),TableCell(),TableCell()],
+        ),
+      ],
+    );
+  }
+}
+
+
+class TableCell extends StatelessWidget {
+  const TableCell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child:
+    Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.neutral50, width: 1)
+      ),
+        child: TextLabel("Data")
+    ));
   }
 }
 
