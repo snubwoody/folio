aa4b2d2e97ef6605b462cfe8c12a9a4e2670848a
waku <wakunguma13@gmail.com>
2026-02-10 16:57:53 -0700

    Add text size styles

--- frontend/folio/lib/style.dart
+++ frontend/folio/lib/style.dart
@@ -1,24 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-// BoxShadow(
-// color: Colors.purple50,
-// blurRadius: 10,
-// offset: Offset(0, 4), // x and y axis offset
-// )
-
-// --shadow-purple-xs: 0 0 2px 0 rgb(75 25 226 / 0.05);
-// --shadow-purple-sm:
-// 0 1px 2px -1px rgb(75 25 226 / 0.1), 0 1px 3px 0px rgb(75 25 226 / 0.1);
-// --shadow-purple-md:
-// 0 2px 4px -2px rgb(75 25 226 / 0.1), 0 4px 6px -1px rgb(75 25 226 / 0.1);
-// --shadow-purple-lg:
-// 0 01px 15px -3px rgb(75 25 226 / 0.1),
-// 0 4px 6px -4px rgb(75 25 226 / 0.1);
-// --shadow-purple-xl:
-// 0 20px 25px -5px rgb(75 25 226 / 0.1),
-// 0 8px 10px -6px rgb(75 25 226 / 0.1);
+// --text-h1--line-height: 1;
+// --text-h2--line-height: 1;
+// --text-h3--line-height: 1;
+// --text-h4--line-height: 1.25;
+// --text-h5--line-height: 1.25;
+// --text-h6--line-height: 1.5;
+// --text-body--line-height: 1.5;
+// --text-sm--line-height: 1.5;
+// --text-xs--line-height: 1.5;
 
 abstract final class Style {
+  static const double textSizeH1 = 64;
+  static const double textSizeH2 = 56;
+  static const double textSizeH3 = 48;
+  static const double textSizeH4 = 36;
+  static const double textSizeH5 = 25;
+  static const double textSizeH6 = 18;
+  static const double textSizeBody = 16;
+  static const double textSizeSm = 13;
+  static const double textSizeXs = 10;
+
   static const List<BoxShadow> shadowXs = [
     BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.05), blurRadius: 2),
   ];
