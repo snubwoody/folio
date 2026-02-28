e0d5dc66eacddabfe122b7576b83a19ea46f8397
waku <wakunguma13@gmail.com>
2026-02-27 18:19:03 -0700

    Add missing value on toolbar group item

--- packages/desktop/src/components/home/transaction/Toolbar.svelte
+++ packages/desktop/src/components/home/transaction/Toolbar.svelte
@@ -4,24 +4,27 @@
     import { CirclePlus } from "@lucide/svelte";
     import { transactionStore } from "$lib/transaction.svelte";
     import { accountStore } from "$lib/account.svelte";
-    import { today,getLocalTimeZone } from "@internationalized/date";
+    import { today, getLocalTimeZone } from "@internationalized/date";
 
-    async function addTransaction(){
+    async function addTransaction() {
         const account = accountStore.accounts[0];
         const date = today(getLocalTimeZone());
         await transactionStore.createExpense({
-            amount:"0.0",
-            date:date.toString(),
-            account:account.id
+            amount: "0.0",
+            date: date.toString(),
+            account: account.id,
         });
     }
 </script>
 
 <Toolbar.Root class="border-t border-neutral-50">
     <Toolbar.Group type="single">
-        <Toolbar.GroupItem class="flex items-center px-2 py-1">
+        <Toolbar.GroupItem
+            value="add-transaction"
+            class="flex items-center px-2 py-1"
+        >
             <TextButton theme="primary" onclick={addTransaction}>
-                <CirclePlus/>
+                <CirclePlus />
                 Add Transaction
             </TextButton>
         </Toolbar.GroupItem>
