034c78f4ff477663f332cab40d2f9b71c4bd0856
waku <wakunguma13@gmail.com>
2026-03-24 16:15:15 -0600

    Disable default context menu

--- crates/folio-desktop/src/lib.rs
+++ crates/folio-desktop/src/lib.rs
@@ -88,6 +88,7 @@ pub async fn run() {
     let app = tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_prevent_default::init())
         .manage(state)
         .setup(setup_app)
         .plugin(tauri_plugin_opener::init())
