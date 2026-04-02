6b69de4c240825d4f73c37e24fa4b17ea3985730
Wakunguma K. <wakunguma13@gmail.com>
2026-04-02 17:57:56 -0600

    feat: add NavigationPanel component

---
+++ packages/desktop/src/components/NavigationPanel.svelte
@@ -0,0 +1,23 @@
+
+<script>
+    import IconButton from "$components/button/IconButton.svelte";
+    import Home from "@lucide/svelte/icons/house";
+    import Chart from "@lucide/svelte/icons/chart-no-axes-combined";
+    import SettingsButton from "$components/settings/SettingsButton.svelte";
+</script>
+
+<nav>
+
+</nav>
+
+<style>
+	nav{
+		width: 100%;
+        max-width: 300px;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		padding: 12px;
+		background-color: var(--color-neutral-25);
+	}
+</style>
