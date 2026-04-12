7b460660dafcf7e082b303b0cda209398ce73291
Wakunguma K. <contact@wakunguma.com>
2026-04-11 18:46:16 -0600

    Add Popover component

---
+++ packages/desktop/src/components/popover/Popover.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import {Popover} from "bits-ui";
+    import type { Snippet } from "svelte";
+
+
+    let {
+        children,
+        open = $bindable(),
+        ...rest
+    }: Popover.RootProps = $props();
+</script>
+
+<Popover.Root bind:open={open} {...rest}>
+    {@render children?.()}
+</Popover.Root>
---
+++ packages/desktop/src/components/popover/PopoverTrigger.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {Popover} from "bits-ui";
+    import type { Snippet } from "svelte";
+
+    interface Props extends Popover.TriggerProps{
+        children: Snippet
+    }
+
+    const {children,...rest}: Props = $props();
+</script>
+
+<Popover.Trigger {...rest}>
+    {@render children()}
+</Popover.Trigger>
