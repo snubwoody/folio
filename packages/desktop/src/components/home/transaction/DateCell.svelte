d83406b4350cdaecd08b8bb93882a0c20ab6b212
Wakunguma K. <contact@wakunguma.com>
2026-04-18 19:03:40 -0600

    Disable close focus in DateCell

--- packages/desktop/src/components/home/transaction/DateCell.svelte
+++ packages/desktop/src/components/home/transaction/DateCell.svelte
@@ -15,29 +15,27 @@
 
     let displayDate = $derived(formatDate(transaction.date));
 
-    // TODO: make editTransaction take date
     const updateDate = async (date: DateValue) => {
         calendarOpen = false;
 
         await transactionStore.editTransaction({ id: transaction.id,transactionDate: date.toString() });
         displayDate = formatDate(toCalendarDate(date));
     };
-    // TODO: change on value
-    // TODO: close focus https://bits-ui.com/docs/components/popover#close-focus
     let date = $derived(transaction.date);
     let calendarOpen = $state(false);
-    // TODO: time might not be updated, check value
 </script>
 
 <TableCell>
     <Popover.Root bind:open={calendarOpen}>
         <Popover.Trigger class="text-left">
-            <time datetime={transaction.date.toString()}>
+            <time datetime={date.toString()}>
                 {displayDate}
             </time>
         </Popover.Trigger>
         <Popover.Portal>
-            <Popover.Content onInteractOutside={() => updateDate(date)}>
+            <Popover.Content
+                onCloseAutoFocus={(e) => e.preventDefault()}
+                onInteractOutside={() => updateDate(date)}>
                 <Calendar bind:value={date} onDateChange={updateDate}/>
             </Popover.Content>
         </Popover.Portal>
