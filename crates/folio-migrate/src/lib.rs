b83aa9705b83050ef2b37125098c779300991820
Wakunguma K. <contact@wakunguma.com>
2026-04-30 11:57:56 -0600

    Add regex

--- crates/folio-migrate/src/lib.rs
+++ crates/folio-migrate/src/lib.rs
@@ -1,10 +1,17 @@
 use std::fs;
+use regex::Regex;
 
 pub fn load_migration(){
+    // TODO: test missing
+    let regex = Regex::new("(?m)^--\\s*migrate:up(\\s*$|\\s+\\S+)")
+        .expect("failed to compile regex");
     // TODO: find the first place with -- migrate:up
-    let path = "../folio-desktop/migrations/category_group.sql";
+    let path = "../folio-desktop/migrations/category_group_column.sql";
     let data = fs::read_to_string(path).unwrap();
-    dbg!(data);
+    for section in regex.find_iter(&data){
+        dbg!(section);
+    }
+    // dbg!(data);
 }
 
 pub fn read_migrations(){
