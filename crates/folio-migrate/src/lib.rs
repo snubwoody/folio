49455d73419202a6077640bf4c5a5eef5445ac11
Wakunguma K. <contact@wakunguma.com>
2026-05-01 11:50:34 -0600

    Load migration from file

--- crates/folio-migrate/src/lib.rs
+++ crates/folio-migrate/src/lib.rs
@@ -1,23 +1,36 @@
 use std::fs;
 use regex::Regex;
 
-pub fn load_migration(){
-    // TODO: test missing
-    let regex = Regex::new("(?m)^--\\s*migrate:up(\\s*$|\\s+\\S+)")
-        .expect("failed to compile regex");
-    // TODO: find the first place with -- migrate:up
-    let path = "../folio-desktop/migrations/category_group_column.sql";
-    let data = fs::read_to_string(path).unwrap();
-    for section in regex.find_iter(&data){
-        dbg!(section);
-    }
-    // dbg!(data);
+pub fn migrate(){
+    let connection_str = std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL not set");
+    let connection = rusqlite::Connection::open(connection_str).unwrap();
 }
 
-pub fn read_migrations(){
-    for entry in fs::read_dir("../folio-desktop/migrations").unwrap(){
-        dbg!(entry);
+pub fn load_migration() -> Vec<String>{
+    let path = "../folio-desktop/migrations/category_group_column.sql";
+    let data = fs::read_to_string(path).unwrap();
+    let mut in_block = false;
+    let mut blocks = vec![];
+    let mut block = String::new();
+    // TODO: seek until next block or EOF
+    for line in data.lines(){
+        let stripped_line = line.replace(" ","");
+        if stripped_line == "--migrate:up" || stripped_line == "--migrate:down"{
+            if !in_block{
+                in_block = true;
+                continue;
+            }
+
+            blocks.push(block.clone());
+            in_block = false
+        }
+
+        if in_block{
+            block.push_str(line)
+        }
     }
+    blocks
 }
 
 #[cfg(test)]
