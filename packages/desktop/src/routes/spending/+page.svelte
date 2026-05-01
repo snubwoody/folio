79e75aeeea0b2a1f8d797e8daf744f4bce12f30a
Wakunguma K. <contact@wakunguma.com>
2026-05-01 16:04:04 -0600

    Add apache echarts to package.json

---
+++ packages/desktop/src/routes/spending/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import * as echarts from "echarts/core";
+    import  {PieChart} from "echarts/charts";
+    import {SVGRenderer} from "echarts/renderers";
+    import { LabelLayout, UniversalTransition } from 'echarts/features';
+    import {
+      TitleComponent,
+      TooltipComponent,
+      GridComponent,
+      DatasetComponent,
+      TransformComponent
+    } from 'echarts/components';
+
+    // Only import certain parts to reduce bundle size
+    echarts.use([
+        LabelLayout,
+        UniversalTransition,
+        PieChart,
+        SVGRenderer,
+        TitleComponent,
+        TooltipComponent,
+        GridComponent,
+        DatasetComponent,
+        TransformComponent,
+    ])
+    let chart = echarts.init(document.getElementById("spending-pie-chart"));
+</script>
+
+<main>
+    <div id="spending-pie-chart"></div>
+</main>
+
+<style>
+
+    
+</style>
