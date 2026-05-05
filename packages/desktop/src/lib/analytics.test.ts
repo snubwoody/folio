77aa681af2a67f0dc295df9a4d60581480bb6865
Wakunguma K. <contact@wakunguma.com>
2026-05-05 15:13:56 -0600

    Refactor spendingAnalytic to return SpendingAnalytic[]

--- packages/desktop/src/lib/analytics.test.ts
+++ packages/desktop/src/lib/analytics.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import type { Category } from "$lib/types"
 import type { Transaction } from "$lib/api/transaction";
 import { getLocalTimeZone, today } from "@internationalized/date";
@@ -11,37 +11,149 @@ import { spendingAnalytics } from "./analytics";
 // - Multiple categories
 // - Exclude incomes and transfers
 // - Exclude incomeStreams
-test("Get total spent in a single category", () => {
-    const currentDate = today(getLocalTimeZone());
-    const category: Category = {
-        id: "C1",
-        title: "Groceries",
-        createdAt: "",
-        isIncomeStream: false
-    };
+// 
+describe("spendingAnalytics", () => {
+    test("get total spent in a single category", () => {
+        const currentDate = today(getLocalTimeZone());
+        const category: Category = {
+            id: "C1",
+            title: "Groceries",
+            createdAt: "",
+            isIncomeStream: false
+        };
+    
+        const transactions: Transaction[] = [
+            {
+                id: "A1",
+                categoryId: category.id,
+                toAccountId: undefined,
+                fromAccountId: undefined,
+                amount: "200",
+                date: currentDate
+            },
+            {
+                id: "A2",
+                categoryId: category.id,
+                toAccountId: undefined,
+                fromAccountId: undefined,
+                amount: "50",
+                date: currentDate
+            }
+        ];
+    
+        const categoryMap = new SvelteMap<string, Category>();
+        categoryMap.set("C1", category);
+    
+        const analytics = spendingAnalytics(transactions, categoryMap, { month: currentDate });
+        expect(analytics[0].total).toStrictEqual(250)
+    });
+    
+    test("exclude transactions without a category", () => {
+        const currentDate = today(getLocalTimeZone());
+        const category: Category = {
+            id: "C1",
+            title: "Groceries",
+            createdAt: "",
+            isIncomeStream: false
+        };
+    
+        const transactions: Transaction[] = [
+            {
+                id: "A1",
+                amount: "500",
+                toAccountId: undefined,
+                fromAccountId: undefined,
+                date: currentDate
+            },
+            {
+                id: "A2",
+                categoryId: category.id,
+                toAccountId: undefined,
+                fromAccountId: undefined,
+                amount: "50",
+                date: currentDate
+            }
+        ];
 
-    const transactions: Transaction[] = [
-        {
-            id: "A1",
-            categoryId: category.id,
-            toAccountId: undefined,
-            fromAccountId: undefined,
-            amount: "200",
-            date: currentDate
-        },
-        {
-            id: "A2",
-            categoryId: category.id,
-            toAccountId: undefined,
-            fromAccountId: undefined,
-            amount: "50",
-            date: currentDate
-        }
-    ];
+        const categoryMap = new SvelteMap<string, Category>();
+        categoryMap.set("C1", category);
 
-    const categoryMap = new SvelteMap<string, Category>();
-    categoryMap.set("C1", category);
+        const analytics = spendingAnalytics(transactions, categoryMap, { month: currentDate });
+        expect(analytics[0].total).toStrictEqual(50)
+    });
 
-    const analytics = spendingAnalytics(transactions, categoryMap, { month: currentDate });
-    expect(analytics.get("Groceries")).toStrictEqual(250)
-});
+    test("only include transactions in current month", () => {
+        const currentDate = today(getLocalTimeZone());
+        const category: Category = {
+            id: "C1",
+            title: "Groceries",
+            createdAt: "",
+            isIncomeStream: false
+        };
+
+        const transactions: Transaction[] = [
+            {
+                id: "A1",
+                amount: "500",
+                toAccountId: undefined,
+                fromAccountId: undefined,
+                date: currentDate.add({ months:1 })
+            },
+            {
+                id: "A2",
+                categoryId: category.id,
+                toAccountId: undefined,
+                fromAccountId: undefined,
+                amount: "50",
+                date: currentDate
+            }
+        ];
+    
+        const categoryMap = new SvelteMap<string, Category>();
+        categoryMap.set("C1", category);
+    
+        const analytics = spendingAnalytics(transactions, categoryMap, { month: currentDate });
+        expect(analytics[0].total).toStrictEqual(50)
+    });
+
+    test("exclude incomes and transfers", () => {
+        const currentDate = today(getLocalTimeZone());
+        const category: Category = {
+            id: "C1",
+            title: "Groceries",
+            createdAt: "",
+            isIncomeStream: false
+        };
+
+        const transactions: Transaction[] = [
+            {
+                id: "T1",
+                amount: "500",
+                toAccountId: "A1",
+                fromAccountId: undefined,
+                date: currentDate.add({ months:1 })
+            },
+            {
+                id: "T2",
+                categoryId: category.id,
+                toAccountId: "A1",
+                fromAccountId: "A2",
+                amount: "50",
+                date: currentDate
+            },
+            {
+                id: "T3",
+                categoryId: category.id,
+                fromAccountId: "A1",
+                amount: "25",
+                date: currentDate
+            }
+        ];
+
+        const categoryMap = new SvelteMap<string, Category>();
+        categoryMap.set("C1", category);
+
+        const analytics = spendingAnalytics(transactions, categoryMap, { month: currentDate });
+        expect(analytics[0].total).toStrictEqual(25)
+    });
+})
--- packages/desktop/src/lib/analytics.ts
+++ packages/desktop/src/lib/analytics.ts
@@ -7,6 +7,14 @@ export type SpendingAnalyticsOptions = {
     month?: CalendarDate;
 };
 
+export type SpendingAnalytic = {
+    category: Category;
+    total: number;
+    /**
+     * A percentage of the total amount, ranging from 0-1.
+     */
+    percentage: number;
+}
 /**
  * Returns a list of total spending per category.
  */
@@ -14,25 +22,45 @@ export function spendingAnalytics(
     transactions: Transaction[],
     categoryMap: SvelteMap<string, Category>,
     opts: SpendingAnalyticsOptions
-) {
+): SpendingAnalytic[] {
     const month = opts.month ?? today(getLocalTimeZone());
     let map = new SvelteMap<string,number>();
     const filteredTransactions = transactions
+        .filter(t => t.categoryId !== undefined)
+        .filter(t => t.toAccountId === undefined) // Exclude incomes and transfers
         .filter(t => isSameMonth(t.date,month));
 
-    for (const t of filteredTransactions){
+    const totalSpent = filteredTransactions
+        .map(t => parseFloat(t.amount))
+        .reduce((prev, curr) => prev + curr, 0);
+
+    for (const t of filteredTransactions) {
         if (!t.categoryId) continue;
 
-        let category = categoryMap.get(t.categoryId)?.title;
-        if (!category) continue;
+        const categoryId = categoryMap.get(t.categoryId)?.id;
+        if (!categoryId) continue;
 
-        let value = map.get(category);
+        let value = map.get(categoryId);
         if (value === undefined){
-            map.set(category,parseFloat(t.amount));
+            map.set(categoryId,parseFloat(t.amount));
             continue;
         }
-        map.set(category, parseFloat(t.amount) + value);
+        map.set(categoryId, parseFloat(t.amount) + value);
     }
 
-    return map
+    const analytics: SpendingAnalytic[] = []
+    for (const [categoryId,total] of map.entries()) {
+        let category = categoryMap.get(categoryId);
+        if (!category) continue;
+
+        const analytic: SpendingAnalytic = {
+            category,
+            total,
+            percentage: total / totalSpent
+        };
+
+        analytics.push(analytic)
+    }
+
+    return analytics;
 }
