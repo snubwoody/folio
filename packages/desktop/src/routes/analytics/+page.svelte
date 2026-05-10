9a8bacb41f5f0347d73d674ecc6d2f99bfee3da0
Wakunguma K. <contact@wakunguma.com>
2026-05-10 17:29:35 -0600

    Add analytics month switch

--- packages/desktop/src/routes/analytics/+page.svelte
+++ packages/desktop/src/routes/analytics/+page.svelte
@@ -12,18 +12,16 @@
     } from "echarts/components";
     import {
         // The component option types are defined with the ComponentOption suffix
+        AriaComponent,
+        LegendComponent,
         type TitleComponentOption,
         type TooltipComponentOption,
         type GridComponentOption,
-        LegendComponent,
         type LegendComponentOption,
         type DatasetComponentOption,
-        AriaComponent,
         type AriaComponentOption
     } from "echarts/components";
-    import type {
-        ComposeOption
-    } from "echarts/core";
+    import type { ComposeOption } from "echarts/core";
     import { transactionStore } from "$lib/stores/transaction.svelte";
     import { getLocalTimeZone, today } from "@internationalized/date";
     import { categoryStore } from "$lib/stores/categories.svelte";
@@ -31,15 +29,15 @@
     import { onMount } from "svelte";
     import CategorySidebar from "$components/analytics/CategorySidebar.svelte";
     import {IconButton} from "$components/button";
+    import {ChevronLeft,ChevronRight} from "@lucide/svelte";
 
-    // Create an Option type with only the required components and charts via ComposeOption
     type ECOption = ComposeOption<
-      | PieSeriesOption
-      | LegendComponentOption
-      | TitleComponentOption
-      | TooltipComponentOption
-      | GridComponentOption
-      | DatasetComponentOption
+        | PieSeriesOption
+        | LegendComponentOption
+        | TitleComponentOption
+        | TooltipComponentOption
+        | GridComponentOption
+        | DatasetComponentOption
         | AriaComponentOption
     >;
 
@@ -58,7 +56,8 @@
         TransformComponent
     ]);
 
-    let analytics = $derived.by(() => spendingAnalytics(transactionStore.transactions,categoryStore.categoryMap,{ month: today(getLocalTimeZone()) }));
+    let month = $state(today(getLocalTimeZone()));
+    let analytics = $derived.by(() => spendingAnalytics(transactionStore.transactions,categoryStore.categoryMap,{ month }));
 
     // TODO: disable start animation
     // TODO: use filled pie chart
@@ -104,20 +103,25 @@
 
     // TODO: show category on hover
     // TODO: check empty chart
-    onMount(() => {
+
+    $effect(()=>{
         // eslint-disable-next-line no-undef
         let chart = echarts.init(document.getElementById("spending-pie-chart"));
         chart.setOption(option);
-    });
+    })
 </script>
 
 <main>
     <section>
-        <header>
-            <h5>Spending breakdown</h5>
-            <div>
-                <IconButton>
-                    Plus
+        <header class="flex items-center justify-between">
+            <h6>Spending breakdown</h6>
+            <div class="flex items-center">
+                <IconButton variant="ghost" onclick={() => month = month.subtract({months: 1})}>
+                    <ChevronLeft/>
+                </IconButton>
+                <p>May 2026</p>
+                <IconButton variant="ghost" onclick={() => month = month.add({months: 1})}>
+                    <ChevronRight/>
                 </IconButton>
             </div>
         </header>
