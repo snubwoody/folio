cecc08c8199a116cda900d37758c259caacdaf5d
Wakunguma K. <contact@wakunguma.com>
2026-05-10 18:05:33 -0600

    Show percentage in category sidebar

--- packages/desktop/src/components/analytics/CategorySidebar.svelte
+++ packages/desktop/src/components/analytics/CategorySidebar.svelte
@@ -13,15 +13,18 @@
 
 <aside>
     <div class="header">
-        <h6 class="text-base font-medium">Categories</h6>
-        <h6 class="text-base font-medium">Total spent</h6>
+        <p class="text-base font-medium">Categories</p>
+        <p class="text-base font-medium">Total spent</p>
     </div>
     <ul class="space-y-5 overflow-auto h-full">
         {#each analytics as analytic (analytic.category.id)}
             <li class="flex items-start justify-between">
                 <div class="w-full space-y-0.5">
                     <p>{analytic.category.title}</p>
-                    <div style:--width={`${analytic.percentage*100}%`} class="bg-surface-primary w-(--width) h-2 rounded-xs"></div>
+                    <div class="flex w-full gap-1 items-center">
+                        <div style:--width={`${analytic.percentage*100}%`} class="bg-surface-primary w-(--width) h-2 rounded-xs"></div>
+                        <p>{Math.round(analytic.percentage * 100)}%</p>
+                    </div>
                 </div>
                 <p>{formatMoney(analytic.total.toString())}</p>
             </li>
