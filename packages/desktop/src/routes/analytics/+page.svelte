d1afa7c347c6e81aa6fda51eaa185a2e75a1a6ea
Wakunguma K. <contact@wakunguma.com>
2026-05-15 23:08:25 -0600

    Remove pie chart internal radius

--- packages/desktop/src/routes/analytics/+page.svelte
+++ packages/desktop/src/routes/analytics/+page.svelte
@@ -11,7 +11,6 @@
         TransformComponent
     } from "echarts/components";
     import {
-        // The component option types are defined with the ComponentOption suffix
         AriaComponent,
         LegendComponent,
         type TitleComponentOption,
@@ -81,7 +80,7 @@
         series: [
             {
                 type: "pie",
-                radius: ["50%", "70%"],
+                // radius: ["50%", "70%"],
                 padAngle: 0.5,
                 avoidLabelOverlap: false,
                 labelLine: {
