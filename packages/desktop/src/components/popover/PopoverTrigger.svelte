c1eeab5dd889dbabff0c67f4ef2421ddb0a54d23
Wakunguma K. <contact@wakunguma.com>
2026-06-15 12:49:27 -0600

    Disable useImportType lint

--- packages/desktop/src/components/popover/PopoverTrigger.svelte
+++ packages/desktop/src/components/popover/PopoverTrigger.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import type { Popover } from "bits-ui";
+    import { Popover } from "bits-ui";
     import type { Snippet } from "svelte";
 
     interface Props extends Popover.TriggerProps {
