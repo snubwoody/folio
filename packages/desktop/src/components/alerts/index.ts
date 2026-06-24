3aeb2cfba6b1972b115a65e24b52ad740f6fd906
Wakunguma K. <contact@wakunguma.com>
2026-06-23 18:28:16 -0600

    Remove frontend code

--- packages/desktop/src/components/alerts/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MessageBar } from "./MessageBar.svelte";
--- packages/desktop/src/components/date/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Calendar } from "./Calendar.svelte";
-export { default as DateField } from "./DateField.svelte";
